100eab2daa2f63d75dda56e990f4228ec8b5dd5b
Alan Kavanagh <alan.kavanagh@gm.com>
2018-12-03 21:36:52 +0000

    Added sitemap generator

--- src/WiProCrawler.java
+++ src/WiProCrawler.java
@@ -1,14 +1,25 @@
 
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * 
@@ -16,8 +27,12 @@ import java.util.logging.Logger;
  *
  */
 public class WiProCrawler {
+
+	private static final Logger LOGGER = Logger.getLogger(WiProCrawler.class.getName());
 	
-    private static final Logger logger = Logger.getLogger(WiProCrawler.class.getName());
+	private static final String DOMAIN = "https://wiprodigital.com";
+	private static final String SITEMAP = "sitemap.xml";
+
 
 	private HashSet<String> internal_links;
 	private HashSet<String> external_links;
@@ -30,7 +45,7 @@ public class WiProCrawler {
 		this.internal_links = new HashSet<String>();
 		this.external_links = new HashSet<String>();
 		this.static_content = new HashSet<String>();
-		logger.fine("WiProCrawler initialised");
+		LOGGER.fine("WiProCrawler: WiProCrawler initialised");
 	}
 
 	/**
@@ -38,70 +53,111 @@ public class WiProCrawler {
 	 * 
 	 * @param URL: URL to parse
 	 */
-	public void getPageLinks(String URL) 
-	{
+	private void getPageLinks(String URL) {
 		if (!internal_links.contains(URL)) {
 			try {
-				logger.fine(String.format("Found new internal link: %s", URL));
+				LOGGER.fine(String.format("WiProCrawler: Found new internal link: %s", URL));
 				internal_links.add(URL);
 
-				Document document = Jsoup.connect(URL).get();
-				Elements linksOnPage = document.select("a[href]");
+				org.jsoup.nodes.Document document = Jsoup.connect(URL).get();
+				org.jsoup.select.Elements linksOnPage = document.select("a[href]");
 
-				for (Element page : linksOnPage) {
+				for (org.jsoup.nodes.Element page : linksOnPage) {
 					if (page.attr("abs:href").startsWith(allowed_domain)) {
 						getPageLinks(page.attr("abs:href"));
 					} 
 					else {
 						/*
-						 * [^\\s]+ - 1 or more anything
-						 * (?i) - Ignore case sensitive for following patterns
-						 * (jpg|png|gif) - Image formats to match
+						 * [^\\s]+ - 1 or more anything 
+						 * (?i) - Ignore case sensitive for following patterns 
+						 * (jpg|png|gif|mp4|jpeg) - Image/Video formats to match
 						 */
 						if (page.attr("abs:href").matches("([^\\s]+(\\.(?i)(jpg|png|gif|mp4|jpeg))$)")) {
-							logger.fine(String.format("Found new static content: %s", page.attr("abs:href")));
+							LOGGER.fine(String.format("WiProCrawler: Found new static content: %s", page.attr("abs:href")));
 							static_content.add(page.attr("abs:href"));
 						} 
 						else {
-							logger.fine(String.format("Found new external link: %s", page.attr("abs:href")));
+							LOGGER.fine(String.format("WiProCrawler: Found new external link: %s", page.attr("abs:href")));
 							external_links.add(page.attr("abs:href"));
 						}
 					}
 				}
 			} 
 			catch (IOException e) {
-				logger.warning("For '" + URL + "': " + e.getMessage());
+				LOGGER.warning("WiProCrawler: For '" + URL + "': " + e.getMessage());
 			}
 		}
 	}
+
+	/**
+	 * Writes a sitemap to sitemap.xml
+	 * @param sitemap: Path to write the sitemap
+	 */
+	private void writeSitemapXML(String sitemap) {
+		try {
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 	
-	public void getInternalLinks() {
-		for (String s : internal_links) {
-			logger.info(String.format("Internal: %s", s));
-		}
-	}
+			Document doc = docBuilder.newDocument();
+			Element rootElement = doc.createElement("urlset");
+			doc.appendChild(rootElement);
+			
+			Attr attr = doc.createAttribute("xmlns");
+			attr.setValue("http://www.sitemaps.org/schemas/sitemap/0.9");
+			rootElement.setAttributeNode(attr);
 	
-	public void getExternalLinks() {
-		for (String s : external_links) {
-			logger.info(String.format("External: %s", s));
-		}
-	}
+			for (String url: internal_links) {
+				Element staff = doc.createElement("url");
+				rootElement.appendChild(staff);
+				attr = doc.createAttribute("loc");
+				Element firstname = doc.createElement("loc");
+				firstname.appendChild(doc.createTextNode(url));
+				staff.appendChild(firstname);
+			}
+			
+			for (String url: external_links) {
+				Element staff = doc.createElement("external-url");
+				rootElement.appendChild(staff);
+				attr = doc.createAttribute("loc");
+				Element firstname = doc.createElement("loc");
+				firstname.appendChild(doc.createTextNode(url));
+				staff.appendChild(firstname);
+			}
+			
+			for (String url: static_content) {
+				Element staff = doc.createElement("static-content");
+				rootElement.appendChild(staff);
+				attr = doc.createAttribute("loc");
+				Element firstname = doc.createElement("loc");
+				firstname.appendChild(doc.createTextNode(url));
+				staff.appendChild(firstname);
+			}
 	
-	public void getStaticContent() {
-		for (String s : static_content) {
-			logger.info(String.format("Static Content: %s", s));
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+			
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File(sitemap));
+	
+			transformer.transform(source, result);
+			LOGGER.info("WiProCrawler: Sitemap saved to sitemap.xml");
+		} 
+		catch (ParserConfigurationException pce) {
+			LOGGER.warning(String.format("WiProCrawler: ParserConfigurationException: %s", pce.getStackTrace().toString()));
+		} 
+		catch (TransformerException tfe) {
+			LOGGER.warning(String.format("WiProCrawler: Transformerexception: %s", tfe.getStackTrace().toString()));
 		}
 	}
 
 	public static void main(String[] args) {
-		String domain = "https://wiprodigital.com";
-		logger.setLevel(Level.FINE);
-		logger.info("Application started");
-		logger.info(String.format("Root domain set to: %s", domain));
-		WiProCrawler wpc = new WiProCrawler(domain);
-		wpc.getPageLinks(domain);
-		wpc.getInternalLinks();
-		wpc.getExternalLinks();
-		wpc.getStaticContent();
+		LOGGER.setLevel(Level.FINE);
+		LOGGER.info("WiProCrawler: Application started");
+		LOGGER.info(String.format("WiProCrawler: Root domain set to: %s", DOMAIN));
+		WiProCrawler wpc = new WiProCrawler(DOMAIN);
+		wpc.getPageLinks(DOMAIN);
+		wpc.writeSitemapXML(SITEMAP);
 	}
 }
